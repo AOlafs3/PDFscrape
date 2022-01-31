5e5cf3084656b4ada028fbb676c47d013289b4fd
Andrew Olafson <86797937+AOlafs3@users.noreply.github.com>
2022-01-31 12:04:55 -0800

    Initial commit

---
+++ PDFscrape/tabula-d2a1ec1e-b141-4768-b512-907bbb34a76f.sh
@@ -0,0 +1,3 @@
+java -jar tabula-java.jar  -a 137.318,47.048,721.778,564.953 -p 5 "$1" 
+java -jar tabula-java.jar  -a 126.608,47.048,529.763,555.008 -p 6 "$1" 
+java -jar tabula-java.jar  -a 163.845,60.885,685.575,718.245 -p 7 "$1" 
